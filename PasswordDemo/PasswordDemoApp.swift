9c1310f9c6a11287196c43eb2867e47e54ca087c
britoscar1 <brito.oscar@uabc.edu.nx>
2026-05-21 11:54:25 -0700

    Initial Commit

---
+++ PasswordDemo/PasswordDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PasswordDemoApp.swift
+//  PasswordDemo
+//
+//  Created by Oscar Artemio Brito Ortiz on 21/05/26.
+//
+
+import SwiftUI
+
+@main
+struct PasswordDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
